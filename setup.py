ad130bdc7de6ed4291573b06a3413d8297ab71d2
Matt Copas <matt@committed.io>
2021-02-24 19:23:16 +0000

    Fix structure

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
+print(find_packages())
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(name="questiongenerator", install_requires=required)
+setup(name="questiongenerator", packages=find_packages(), install_requires=required)
